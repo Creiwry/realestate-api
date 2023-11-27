b26d9b516d98badd9820c505a9afe443618f43c1
Creiwry <jay.gardos@outlook.com>
2023-11-27 15:35:57 +0100

    feat(testing): add aditional attributes to factories

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,5 +1,7 @@
 FactoryBot.define do
   factory(:user) do
+    first_name { Faker::Name.first_name }
+    last_name { Faker::Name.last_name }
     email { Faker::Internet.email }
     password { 'password' }
   end
@@ -11,7 +13,8 @@ FactoryBot.define do
     location { Faker::Address.city }
     description { Faker::Lorem.paragraphs.join(' ') }
     area { Faker::Number.between(from: 20, to: 200) }
-    number_of_rooms { Faker::Number.between(from: 1, to: 10) }
+    number_of_rooms { Faker::Number.between(from: 4, to: 10) }
+    number_of_rooms { Faker::Number.between(from: 1, to: 4) }
     furnished { [true, false].sample }
     terrace { [true, false].sample }
     basement { [true, false].sample }
