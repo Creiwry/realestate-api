0231fb51b5e93f68b39c6ce0dcb2423f8533b5f0
Creiwry <jay.gardos@outlook.com>
2023-11-27 14:20:39 +0100

    config(testing): set up rspec

--- Gemfile
+++ Gemfile
@@ -44,7 +44,8 @@ gem 'jsonapi-serializer'
 
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
-  gem "rspec"
+  gem "rspec-rails"
+  gem "factory_bot_rails"
   gem "faker", "~> 3.2"
   gem "debug", platforms: %i[ mri mingw x64_mingw ]
 end
---
+++ spec/factories.rb
@@ -0,0 +1,28 @@
+FactoryBot.define do
+  factory(:user) do
+    email { Faker::Internet.email }
+    password { 'password' }
+  end
+end
+
+FactoryBot.define do
+  factory(:property) do
+    name { Faker::Lorem.words(number: 5).join(' ') }
+    location { Faker::Address.city }
+    description { Faker::Lorem.paragraphs.join(' ') }
+    area { Faker::Number.between(from: 20, to: 200) }
+    number_of_rooms { Faker::Number.between(from: 1, to: 10) }
+    furnished { [true, false].sample }
+    terrace { [true, false].sample }
+    basement { [true, false].sample }
+    renting { [true, false].sample }
+    user { create(:user) }
+  end
+end
+
+FactoryBot.define do
+  factory(:user_property) do
+    user { create(:user) }
+    property { create(:property) }
+  end
+end
