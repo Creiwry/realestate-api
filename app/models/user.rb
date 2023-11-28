4d10676a4c2eca46d3e02fc34e1fe2ff396d8039
Creiwry <jay.gardos@outlook.com>
2023-11-29 00:18:24 +0100

    fix(controller): add attributes to devise registration controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,4 +13,6 @@ class User < ApplicationRecord
 
   validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
   validates_with Validators::PasswordRegexValidator
+  validates :first_name, presence: true
+  validates :last_name, presence: true
 end
