85ddb0d9aa019c0880dadfd5207aa578ac37f43c
Creiwry <jay.gardos@outlook.com>
2023-11-29 19:58:07 +0100

    fix(devise): create custom failure app for json error rendering

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -226,6 +226,9 @@ Devise.setup do |config|
   # change their passwords.
   config.reset_password_within = 6.hours
 
+  config.warden do |manager|
+    manager.failure_app = CustomFailureApp::FailureApp
+  end
   # When set to false, does not sign a user in automatically after their password is
   # reset. Defaults to true, so a user is signed in automatically after a reset.
   # config.sign_in_after_reset_password = true
